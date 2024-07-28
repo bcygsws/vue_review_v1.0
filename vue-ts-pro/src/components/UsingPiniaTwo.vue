d932f6ab99f5a145bf9df72371e964f6bb1de179
bcygsws <bcygswspring168@gmail.com>
2024-07-28 18:57:20 +0800

    :memo: Vue回顾-pinia状态管理器的使用

--- vue-ts-pro/src/components/UsingPiniaTwo.vue
+++ vue-ts-pro/src/components/UsingPiniaTwo.vue
@@ -5,14 +5,23 @@
 -->
 <template>
   <div>
-    <h2></h2>
-    <p>名字：{{ personStore.getFullName}}</p>
+    <h3>三、UsingPinia组件</h3>
+    <p>名字：{{ personStore.getFullName }}</p>
+    <button @click="clickHandler">修改UsingPinia中的count值</button>
+    <p>count值：{{ counterStore.count }}</p>
+
   </div>
 </template>
 <script lang="ts" setup>
 import usePerson from "@/store/user";
+import useCounter from "@/store/counter.ts";
 
 const personStore = usePerson();
+const counterStore = useCounter();
+// 直接修改count值
+const clickHandler = () => {
+  counterStore.count++;
+}
 
 
 </script>
